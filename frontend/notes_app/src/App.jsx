f8b408e7622cf27450bb188fb5e153a55e4d2545
shree-saroj <s.saroj0048@gmail.com>
2025-03-22 15:15:30 +0545

    case check

--- frontend/notes_app/src/App.jsx
+++ frontend/notes_app/src/App.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
-import Login from "./pages/Login/login";
-import SignUp from "./pages/SignUp/signUp";
+import Login from "./pages/Login/Login";
+import SignUp from "./pages/SignUp/SignUp";
 
 const routes = (
   <Router>
     <Routes>
       <Route path="/" exact element={<Home />} />
-      <Route path="/login" exact element={<Login />} />
-      <Route path="/signup" exact element={<SignUp />} />
+      <Route path="/Login" exact element={<Login />} />
+      <Route path="/SignUp" exact element={<SignUp />} />
     </Routes>
   </Router>
 );
